36df4f2842ed51f2b95dabc30b64207e32608b53
sunnry <sunhaihuan@gmail.com>
2016-05-19 13:05:49 +0800

    add wx.pancel in MainFrame

--- MainFrame.py
+++ MainFrame.py
@@ -6,7 +6,8 @@ import wx
 def create(parent):
     return mainFrame(parent)
 
-[wxID_MAINFRAME] = [wx.NewId() for _init_ctrls in range(1)]
+[wxID_MAINFRAME, wxID_MAINFRAMEMAINFRAMEPANEL1, 
+] = [wx.NewId() for _init_ctrls in range(2)]
 
 [wxID_MAINFRAMEFILEITEMS0, wxID_MAINFRAMEFILEITEMS1, 
 ] = [wx.NewId() for _init_coll_File_Items in range(2)]
@@ -39,11 +40,15 @@ class mainFrame(wx.Frame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_MAINFRAME, name=u'mainFrame',
-              parent=prnt, pos=wx.Point(578, 225), size=wx.Size(757, 489),
+              parent=prnt, pos=wx.Point(432, 102), size=wx.Size(1121, 607),
               style=wx.DEFAULT_FRAME_STYLE, title=u'ialphastock')
         self._init_utils()
-        self.SetClientSize(wx.Size(749, 462))
+        self.SetClientSize(wx.Size(1113, 580))
         self.SetMenuBar(self.menuBar)
+
+        self.MainFramePanel1 = wx.Panel(id=wxID_MAINFRAMEMAINFRAMEPANEL1,
+              name=u'MainFramePanel1', parent=self, pos=wx.Point(0, 0),
+              size=wx.Size(1113, 580), style=wx.TAB_TRAVERSAL)
 
     def __init__(self, parent):
         self._init_ctrls(parent)
